5c12ff92f8178ee642562a1f59ce4abcd962681c
sa-bri-na <huber_sabrina@me.com>
2024-08-05 18:18:01 +0200

    add user weight to app

--- hook/useHealthData.jsx
+++ hook/useHealthData.jsx
@@ -3,7 +3,10 @@ import AppleHealthKit from 'react-native-health';
 
 const permissions = {
     permissions: {
-        read: [AppleHealthKit.Constants.Permissions.Steps],
+        read: [
+            AppleHealthKit.Constants.Permissions.Steps,
+            AppleHealthKit.Constants.Permissions.Weight,
+        ],
         write: [],
     },
 };
@@ -11,6 +14,7 @@ const permissions = {
 const useHealthData = () => {
     const [hasPermission, setHasPermission] = useState(false);
     const [steps, setSteps] = useState(null);
+    const [weight, setWeight] = useState(null)
 
     const initAppleHealthKit = () => {
         AppleHealthKit.initHealthKit(permissions, (err) => {
@@ -41,15 +45,46 @@ const useHealthData = () => {
         });
     }
 
+    const getWeight = () => {
+        if (!hasPermission) {
+            return;
+        }
+
+        const options = {
+            unit: "pound"
+        }
+
+        AppleHealthKit.getLatestWeight(options, (err, results) => {
+            if(err){
+                console.log('Error getting the latest weight: ', err);
+                return;
+            }
+            if (!results || !results.value) {
+                console.log('No weight data available');
+                setWeight(null);
+                return;
+            }
+
+            setWeight(convertPoundsToKilograms(results.value));
+        })
+    }
+
+    const convertPoundsToKilograms = (pounds) => {
+        const kilograms = pounds * 0.45359237;
+        return parseFloat(kilograms.toFixed(2));
+    };
+
+
     useEffect(() => {
         initAppleHealthKit();
     }, []);
 
     useEffect(() => {
         getSteps();
+        getWeight();
     }, [hasPermission]);
 
-    return { steps };
+    return { steps, weight };
 };
 
 export default useHealthData;
